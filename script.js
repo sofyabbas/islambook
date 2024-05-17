4731cf28d3d6e72de63eb2f3f4ed8ff9ac421bf8
Sofy <sofyebnabbas@gmail.com>
2024-05-17 15:51:10 +0200

    i finally finshed clicks and reset counter values

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
+function resetCounter(element) {
+    const parentDiv = element.closest('.counters');
+    const counterElement = parentDiv.querySelector('.counter .countP');
+    const maxCounterElement = parentDiv.querySelector('.zeker_repeat .max-counter');
 
-// let zekerNums = document.getElementById("max-counter").innerHTML;
+    let maxCounterValue = parseInt(maxCounterElement.innerText) + 1;
 
-// document.getElementById("counter-down").addEventListener("click", function () {
-//     if (parseInt(document.getElementById("counter").innerHTML) > 0) {
-//         let tasbeh = document.getElementById("counter").innerHTML;
-//         tasbeh -= 1;
-//         document.getElementById("counter").innerHTML = "0" + tasbeh;
-//         document.getElementById(counter-down).style.backgroundColor = '#3498db';
-//         alert("done");
-//     }
-//     // if (document.getElementById("counter").innerHTML === "00") {
+    counterElement.innerText = "0" + maxCounterValue.toString();
 
-//     // }
-// },1000);
+}
 
-// //resetting counter 
-// document.getElementById("reloading").addEventListener("click", function (event) {
-//     //the following code to stop triggering click on div
-//     event.stopPropagation();
-//     document.getElementById("counter").innerHTML = zekerNums;
-// },1000);
+const counters = document.querySelectorAll('.counters');
 
-document.querySelectorAll(".counters").forEach(div => {
-    div.addEventListener("click", function () {
-  alert("Hello");
-    })
+counters.forEach(counter => {
+    const counterElement = counter.querySelector('.counter .countP');
+
+    counter.addEventListener('click', function () {
+        let counterValue = parseInt(counterElement.innerText);
+        if (counterValue > 0) {
+            counterValue--;
+            counterElement.innerText ="0" +counterValue.toString();
+        }
+    });
 });
